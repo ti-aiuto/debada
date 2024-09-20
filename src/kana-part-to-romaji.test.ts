5ef3bd78a59b8b920f1a2c6a97470c25770eda3e
ti-aiuto <ti.aiuto@outlook.jp>
2024-09-21 01:14:18 +0900

    Update kana-part-to-romaji.test.ts

--- src/kana-part-to-romaji.test.ts
+++ src/kana-part-to-romaji.test.ts
@@ -3,5 +3,14 @@ import {kanaPartToRomaji} from './kana-part-to-romaji';
 describe('kanaPartToRomaji', () => {
   test('対応する文字のローマ字に変換すること', () => {
     expect(kanaPartToRomaji('あ')).toEqual(['a']);
+    expect(kanaPartToRomaji('つ')).toEqual(['tu', 'tsu']);
+    expect(kanaPartToRomaji('ぁ')).toEqual(['xa', 'la']);
+    expect(kanaPartToRomaji('ふぁ')).toEqual([
+      'fa',
+      'huxa',
+      'hula',
+      'fuxa',
+      'fula',
+    ]);
   });
 });
