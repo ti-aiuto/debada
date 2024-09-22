b59a78388505ced9d14b100ce87e477fb644e88c
ti-aiuto <ti.aiuto@outlook.jp>
2024-09-22 16:39:32 +0900

    :heavy_plus_sign: 追加

---
+++ src/app/composables/typing-game.ts
@@ -0,0 +1,69 @@
+import {ref} from 'vue';
+import {TypingWord} from '../../kana2romaji/typing-word';
+
+export function typingGame(words: string[]) {
+  const currenWordIndex = ref(-1);
+  const correctCount = ref(0);
+  const wrongCount = ref(0);
+  const renzokuCorrectCount = ref(0);
+  const koremadeUttaRoamji = ref('');
+  const nokoriRomaji = ref('');
+  const hasCompletedWord = ref(false);
+  const hasCompletedGame = ref(false);
+
+  let currentTypingWord: TypingWord | undefined = undefined;
+
+  const typeKey = function (key: string): boolean {
+    if (!currentTypingWord) {
+      throw new Error('currentTypingWordが未定義');
+    }
+
+    const result = currentTypingWord.typeKey(key);
+    syncVars();
+
+    if (result) {
+      correctCount.value += 1;
+      renzokuCorrectCount.value += 1;
+    } else {
+      wrongCount.value += 1;
+      renzokuCorrectCount.value = 0;
+    }
+
+    return result;
+  };
+
+  function prepareNextQuestion(): boolean {
+    currenWordIndex.value += 1;
+
+    const nextWord = words[currenWordIndex.value];
+    if (!nextWord) {
+      hasCompletedGame.value = true;
+      return false;
+    }
+
+    currentTypingWord = new TypingWord(nextWord);
+    syncVars();
+
+    return true;
+  }
+
+  function syncVars() {
+    if (!currentTypingWord) {
+      return;
+    }
+
+    koremadeUttaRoamji.value = currentTypingWord.koremadeUttaRoamji();
+    nokoriRomaji.value = currentTypingWord.nokoriRomaji();
+    hasCompletedWord.value = currentTypingWord.hasCompleted();
+  }
+
+  return {
+    correctCount,
+    wrongCount,
+    renzokuCorrectCount,
+    typeKey,
+    prepareNextQuestion,
+    hasCompletedWord,
+    hasCompletedGame,
+  };
+}
