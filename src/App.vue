e8109377c00d9566ac96810c2287a018dee065e1
ti-aiuto <ti.aiuto@outlook.jp>
2024-09-22 20:35:28 +0900

    :pencil2: もう少し長い単語にする

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { typingGame } from './composables/typing-game'
 import { onMounted, onUnmounted } from 'vue'
 
-const questions = ['あか', 'あお', 'き'];
+const questions = ['れいとれすぽんす', 'にゅーあぎゅめんと', 'どろーひてい'];
 
 const {
   correctCount,
@@ -11,6 +11,7 @@ const {
   typeKey,
   proceedToNextQuestion,
   hasCompletedWord,
+  hasCompletedGame,
   koremadeUttaRoamji,
   nokoriRomaji,
   currentQuestionIndex,
@@ -19,7 +20,7 @@ const {
 function keyDownListener(event: KeyboardEvent) {
   typeKey(event.key);
   if (hasCompletedWord.value) {
-    // proceedToNextQuestion();
+    proceedToNextQuestion()
   }
 }
 
@@ -31,19 +32,32 @@ onUnmounted(() => document.removeEventListener('keydown', keyDownListener))
 <template>
   <div class="screen">
     <div>
-      <div>
+      <div class="m-plus-rounded-1c-regular">
         正解タイプ数：{{ correctCount }}、連続正解タイプ数：{{ renzokuCorrectCount }}、間違いタイプ数：{{ wrongCount }}
       </div>
-      <div>
+      <div class="m-plus-rounded-1c-regular">
         {{ currentQuestionIndex + 1 }} / {{ questions.length }}問
       </div>
+      <div v-if="hasCompletedGame">
+        おわり
+      </div>
     </div>
-    <strong>{{ koremadeUttaRoamji }}</strong>{{ nokoriRomaji }}
+    <div class="question-area m-plus-rounded-1c-regular">
+      <strong>{{ koremadeUttaRoamji.toUpperCase() }}</strong>{{ nokoriRomaji.toUpperCase() }}
+    </div>
   </div>
 </template>
 
 <style scoped>
 .screen {
   border: 2px solid #000;
+  width: 640px;
+  height: 480px;
+  margin: auto;
+}
+
+.question-area {
+  text-align: center;
+  font-size: 30px;
 }
 </style>
