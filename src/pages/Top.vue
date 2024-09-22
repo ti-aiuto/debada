9459fd384d6046f64461336b91af8a0bab6f7ba1
ti-aiuto <ti.aiuto@outlook.jp>
2024-09-23 00:45:32 +0900

    :heavy_plus_sign: ボタン実装

--- src/pages/Top.vue
+++ src/pages/Top.vue
@@ -2,11 +2,21 @@
   <div>
     <img :src="bgImageUrl" class="bg-image">
     <div>
-      <RouterLink to="/prepare">ゲーム開始</RouterLink>
+      <RouterLink to="/prepare">
+        <img :src="startButton" class="image-button start-button">
+      </RouterLink>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import bgImageUrl from '../assets/background/title-screen.svg';
+import startButton from '../assets/buttons/start-button.svg';
 </script>
+
+<style scoped>
+
+.start-button {
+  width: 300px;
+}
+</style>
