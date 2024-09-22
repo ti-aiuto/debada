cd2d24635f7ab24ef75cdd47eb11ef66a935d824
ti-aiuto <ti.aiuto@outlook.jp>
2024-09-23 00:59:35 +0900

    :pencil2: タイトル遷移ボタン実装

--- src/pages/Rule.vue
+++ src/pages/Rule.vue
@@ -2,11 +2,24 @@
   <div>
     <img :src="bgImageUrl" class="bg-image">
     <div>
-      <RouterLink to="/">TOPへ</RouterLink>
+      <RouterLink to="/" class="to-title-button">
+        <img :src="toTitleButton" class="image-button">
+      </RouterLink>
     </div>
 </div>
 </template>
 
 <script setup lang="ts">
 import bgImageUrl from '../assets/background/rule-screen.png';
+import toTitleButton from '../assets/buttons/to-title-button.png';
 </script>
+
+<style scoped>
+.to-title-button {
+  width: 240px;
+  display: block;
+  position: absolute;
+  top: 400px;
+  left: calc(320px - 120px);
+}
+</style>
