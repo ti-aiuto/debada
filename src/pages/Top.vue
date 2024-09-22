e48c48633219647cc838c361229446ca746035df
ti-aiuto <ti.aiuto@outlook.jp>
2024-09-23 00:00:22 +0900

    :heavy_plus_sign: トップ画像実装

--- src/pages/Top.vue
+++ src/pages/Top.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    トップ画面
+    <img :src="bgImageUrl" class="bg-image">
     <div>
       <RouterLink to="/prepare">ゲーム開始</RouterLink>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import bgImageUrl from '../assets/title-screen.svg';
+</script>
