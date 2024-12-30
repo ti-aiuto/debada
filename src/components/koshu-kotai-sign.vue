91b98abcc77ab717006213ffff2a11dde770e014
ti-aiuto <ti.aiuto@outlook.jp>
2024-12-31 00:55:06 +0900

    :heavy_plus_sign: 攻守交替の素材追加

---
+++ src/components/koshu-kotai-sign.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import koshuKotaiImage from '../assets/sprites/koshu-kotai.png';
+
+let imageTimer: any = null;
+const imageClass = ref('');
+
+function show() {
+  imageClass.value = '';
+  clearTimeout(imageTimer);
+
+  imageTimer = setTimeout(() => {
+    imageClass.value = '';
+  }, 1000);
+  imageClass.value = 'image-visible';
+}
+
+defineExpose({ show });
+</script>
+
+<template>
+  <img :src="koshuKotaiImage" class="image" :class="imageClass">
+</template>
+
+<style scoped>
+.image {
+  width: 571px;
+  height: 299px;
+  opacity: 0;
+}
+
+@keyframes image-animate {
+  0% {
+    opacity: 0;
+    transform: scale(0);
+  }
+
+  25% {
+    opacity: 1;
+    transform: scale(1);
+  }
+
+  75% {
+    opacity: 1;
+    transform: scale(1);
+  }
+
+  100% {
+    opacity: 0;
+    transform: scale(0);
+  }
+}
+
+.image-visible {
+  animation-name: image-animate;
+  animation-duration: 1s;
+  animation-iteration-count: 1;
+}
+</style>
