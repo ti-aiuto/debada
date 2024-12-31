f750f863aee2378a517819fd483ac28616d83fdb
ti-aiuto <ti.aiuto@outlook.jp>
2024-12-31 13:30:18 +0900

    :pencil2: スコア表示

--- src/pages/Result.vue
+++ src/pages/Result.vue
@@ -2,16 +2,29 @@
   <div>
     <img :src="bgImageUrl" class="bg-image">
     <div>
+      <div>
+        {{ Math.floor(tweened.score) }}
+      </div>
+
       <RouterLink to="/" class="to-title-button">
         <img :src="toTitleButton" class="image-button">
       </RouterLink>
     </div>
-</div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import bgImageUrl from '../assets/background/result-screen.webp';
 import toTitleButton from '../assets/buttons/to-title-button.png';
+import gsap from 'gsap'
+import { reactive } from 'vue'
+
+const tweened = reactive({
+  score: 0
+})
+
+gsap.to(tweened, { duration: 1, score: 200 })
+
 </script>
 
 <style scoped>
