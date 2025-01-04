ecc100cc62f47cb9ab3c5b80ddde3abd180d77d7
ti-aiuto <ti.aiuto@outlook.jp>
2025-01-04 18:06:38 +0900

    ✏️ 表記変更

--- src/debada-game/format-per-key-wrong-count.test.ts
+++ src/debada-game/format-per-key-wrong-count.test.ts
@@ -3,9 +3,9 @@ import { formatPerKeyWrongCount } from "./format-per-key-wrong-count";
 describe('formatPerKeyWrongCount', () => {
   it('戻り値の確認', () => {
     expect(formatPerKeyWrongCount({})).toEqual('');
-    expect(formatPerKeyWrongCount({ b: 1 })).toEqual('b:1回');
-    expect(formatPerKeyWrongCount({ b: 2, a: 3 })).toEqual('a:3回 b:2回');
-    expect(formatPerKeyWrongCount({ b: 2, a: 3, c: 4, d: 5 })).toEqual('d:5回 c:4回 a:3回 b:2回');
-    expect(formatPerKeyWrongCount({ b: 2, a: 3, c: 4, d: 5, e: 6 })).toEqual('e:6回 d:5回 c:4回 a:3回 b:2回ほか');
+    expect(formatPerKeyWrongCount({ b: 1 })).toEqual('B:1回');
+    expect(formatPerKeyWrongCount({ b: 2, a: 3 })).toEqual('A:3回 B:2回');
+    expect(formatPerKeyWrongCount({ b: 2, a: 3, c: 4, d: 5 })).toEqual('D:5回 C:4回 A:3回 B:2回');
+    expect(formatPerKeyWrongCount({ b: 2, a: 3, c: 4, d: 5, e: 6 })).toEqual('E:6回 D:5回 C:4回 A:3回 B:2回ほか');
   })
 })
--- src/debada-game/format-per-key-wrong-count.ts
+++ src/debada-game/format-per-key-wrong-count.ts
@@ -4,7 +4,7 @@ export function formatPerKeyWrongCount(perKeyWrongCount: {
   [key: string]: number;
 }): string {
   const sorted = sortBy(Object.entries(perKeyWrongCount), row => -row[1])
-    .map(row => `${row[0]}:${row[1]}回`)
+    .map(row => `${row[0].toUpperCase()}:${row[1]}回`)
   if (sorted.length > 4) {
     return sorted.slice(0, 5).join(' ') + 'ほか'
   } else {
