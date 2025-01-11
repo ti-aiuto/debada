8c562e11a94d11d483792680134d1c3d034704e4
ti-aiuto <ti.aiuto@outlook.jp>
2025-01-11 22:07:55 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
-  <main class="screen">
-    <RouterView />
-  </main>
+  <div>
+    <main class="screen">
+      <RouterView />
+    </main>
+
+    <div class="m-plus-rounded-1c-regular bug-message">
+      バグ報告・新たな用語や文章の追加の等の提案は<a
+        href="https://docs.google.com/forms/d/e/1FAIpQLSeSoh16hl58ROD5Mi7hSNwd94c29gcrk7F8Q1BgNHsDYTjwsA/viewform?usp=sf_link"
+        target="_blank">こちら</a>から<br>
+      ちなみに<a href="https://github.com/ti-aiuto/debada/tree/main/src" target="_blank">こちら</a>からソースコードが見れます
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -11,5 +20,16 @@
   height: 480px;
   margin: auto;
   position: relative;
+}
+
+.bug-message {
+  margin: 32px;
+  font-size: 12px;
+  text-align: center;
+  color: #aaa;
+}
+
+.bug-message a {
+  color: #aaa;
 }
 </style>
