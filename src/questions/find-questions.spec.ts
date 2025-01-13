e9445a31651046545a79df6cc9a19e9de0638f05
ti-aiuto <ti.aiuto@outlook.jp>
2025-01-13 19:59:48 +0900

    ✅ テスト追加

---
+++ src/questions/find-questions.spec.ts
@@ -0,0 +1,21 @@
+import {findQuestions} from './find-questions';
+
+describe('findQuestions', () => {
+  describe('mode=typing-practice', () => {
+    it('例外にならずにも戻り値を返せること', () => {
+      const result = findQuestions('typing-practice');
+      expect(result.selectedEasyQuestions.length).toBeGreaterThanOrEqual(1);
+      expect(result.selectedMiddleQuestions.length).toBeGreaterThanOrEqual(1);
+      expect(result.selectedHardQuestions.length).toBeGreaterThanOrEqual(1);
+    });
+  });
+
+  describe('mode=word-quiz', () => {
+    it('例外にならずにも戻り値を返せること', () => {
+      const result = findQuestions('word-quiz');
+      expect(result.selectedEasyQuestions.length).toBeGreaterThanOrEqual(1);
+      expect(result.selectedMiddleQuestions.length).toBeGreaterThanOrEqual(1);
+      expect(result.selectedHardQuestions.length).toBeGreaterThanOrEqual(1);
+    });
+  });
+});
