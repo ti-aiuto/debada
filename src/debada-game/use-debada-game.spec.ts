b2405fdaa472254f0f39a9209eb0ac607d67f752
ti-aiuto <ti.aiuto@outlook.jp>
2025-01-19 23:24:24 +0900

    ✅ テスト追加中

---
+++ src/debada-game/use-debada-game.spec.ts
@@ -0,0 +1,44 @@
+import {useDebadaGame} from './use-debada-game';
+
+describe('useDebadaGame', () => {
+  function build({
+    selectedEasyQuestions = [
+      {label: 'か', kana: 'か'},
+      {label: 'き', kana: 'き'},
+      {label: 'く', kana: 'く'},
+      {label: 'け', kana: 'け'},
+      {label: 'こ', kana: 'こ'},
+      {label: 'さ', kana: 'さ'},
+      {label: 'し', kana: 'し'},
+    ],
+    selectedMiddleQuestions = [
+      {label: 'た', kana: 'た'},
+      {label: 'ち', kana: 'ち'},
+      {label: 'つ', kana: 'つ'},
+      {label: 'て', kana: 'て'},
+    ],
+    selectedHardQuestions = [
+      {label: 'な', kana: 'な'},
+      {label: 'に', kana: 'に'},
+      {label: 'ぬ', kana: 'ぬ'},
+      {label: 'ね', kana: 'ね'},
+      {label: 'の', kana: 'の'},
+    ],
+    notifyGameEvent = jest.fn(),
+  } = {}) {
+    return useDebadaGame({
+      selectedEasyQuestions,
+      selectedMiddleQuestions,
+      selectedHardQuestions,
+      notifyGameEvent,
+    });
+  }
+
+  describe('質問の個数が足りない場合', () => {
+    it('例外になること', () => {
+      expect(() => build({selectedEasyQuestions: []})).toThrow();
+      expect(() => build({selectedMiddleQuestions: []})).toThrow();
+      expect(() => build({selectedHardQuestions: []})).toThrow();
+    });
+  });
+});
